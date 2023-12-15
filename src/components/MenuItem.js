f061b1f819839e0fb63d6914a00a2a969f5226a0
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-12-15 10:10:42 +0000

    final edit

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -3,16 +3,16 @@ import  ReactDOM  from 'react';
 
 const MenuItem = (props)=>{
 
-
+     
     return(
         <div className = "main-container">
             <div className='food-image' >
                <img src={props.item.img}></img>
             </div>
             <div className='food-description'>
-                <div>
-                    <h3>{props.item.title}</h3>
-                    <h3>${props.item.price}</h3>
+                <div className='item-head'>
+                    <h3 className='start'>{props.item.title}</h3>
+                    <h3 className='end'>${props.item.price}</h3>
                 </div>
                 <hr/>
                 <div>
